f75fdd5cb5dca1165c374edb11e4df6dbeeccf9d
NocSix <davianstewart223@gmail.com>
2022-08-16 14:04:11 -0500

    update

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+
+import datetime
+
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return  render_template("index.html")
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        print(entry_content, formatted_date)
+    return render_template("index.html")
+
+
+
